59c0a28f9b6b9e4e14f85453ae44f2219fc24847
MariaLight <mdlight@mail.ru>
2021-03-24 08:03:40 +0300

    header & menu fixed

--- js/main.js
+++ js/main.js
@@ -14,18 +14,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     burger.addEventListener('click', function () {
         menu.classList.toggle('is-active');
+        document.querySelector('.header-nav__list').classList.toggle('container');
+        document.querySelector('.burger-img-x').classList.toggle('is-open');
+        document.querySelector('.burger-img').classList.toggle('closed');
+
     })
 
 
     const outlineBtn = document.querySelector('.banner__btn-outline');
     const mainBtn = document.querySelector('.banner__btn');
-    mainBtn.addEventListener('focus', function (){
+    mainBtn.addEventListener('focus', function () {
         outlineBtn.classList.toggle('banner__btn-outline-active');
     })
 
-    document.querySelector(".header-styles__btn").addEventListener('click', function(){
-        document.querySelector(".realism").classList.toggle('is-open');
-    })
 
-    new SimpleBar(document.querySelector('.realism'));
+    const drops = document.querySelectorAll('.nav__list--dropdown');
+    for (let i = 0; i < drops.length; ++i) {
+        new SimpleBar(drops[i]);
+    }
 })
